5a250d28513927103247a399e5f7e383c9ee47d6
JindunLAPTOP <Jindun@LAPTOP-KSH4E688>
2017-05-09 17:40:49 +0200

    Push de l'infini

--- src/test.m
+++ src/test.m
@@ -1,25 +1,25 @@
 function tab = test
-    global sVN;
-    global sRANDU;
-    global sMT;
-    global sSM;
-
-    % à initialiser à début
-%     graine = 215;
-%     sVN = graine;
-%     sRANDU = graine;
-%     sMT = graine;
-%     sSM = graine;
-    
-    k = 1000;
-    b = 20;
-    m = 2^31-1;
-    nbInit = 100;
-    
-    tab1 = VonNeumann(k);
-    tab2 = RANDU(k);
-    tab3 = MersenneTwister(k);
-    tab4 = StandardMinimal(k);
+%%    global sVN;
+%%    global sRANDU;
+%%    global sMT;
+%%    global sSM;
+%%
+%%    % ï¿½ initialiser ï¿½ dï¿½but
+%%     graine = 215;
+%%     sVN = graine;
+%%     sRANDU = graine;
+%%     sMT = graine;
+%%     sSM = graine;
+%%    
+%%    k = 1000;
+%%    b = 20;
+%%    m = 2^31-1;
+%%    nbInit = 100;
+%%    
+%%    tab1 = VonNeumann(k);
+%%    tab2 = RANDU(k);
+%%    tab3 = MersenneTwister(k);
+%%    tab4 = StandardMinimal(k);
      
     %% Q2 : Affichage des histogrammes
 %     figure(1);
@@ -31,7 +31,7 @@ function tab = test
 %     figure(4);
 %     hist(tab4, b);
 
-%     %% Q3 : Test de fréquence monobit
+%     %% Q3 : Test de frï¿½quence monobit
 %     Pvaleur1 = Frequency(tab1, 31);
 %     Pvaleur2 = Frequency(tab2, 31);
 %     Pvaleur3 = zeros(1, nbInit);
@@ -93,41 +93,43 @@ function tab = test
 %     figure(1);
 %     hist(tabNom,20);
     
-%                 %% Q8 : Inversion et rejet
-%     inv=zeros(1,1000);
-%     rej=zeros(1,1000);
-% 
-%     for i=1:1000
-%         inv(1,i) =Inversion();
-%         rej(1,i) =Rejet();
-%     end
-%     
-%     c=2/(log(2)*log(2));
-%     x=0:0.05:1;
-%     y= c * (log(1+x)/(1+x));
-%     
-%     figure(3);
-%     plot(x,y);
-%     
-%     
-%     figure(1);
-%     hist(inv,20);
-%     figure(2);
-%     hist(rej,20);
+                 %% Q8 : Inversion et rejet
+     inv=zeros(1,1000);
+     rej=zeros(1,1000);
+ 
+     for i=1:1000
+         inv(1,i) =Inversion();
+         rej(1,i) =Rejet();
+     end
+     
+     c=2/(log(2)*log(2));
+     x=0:0.05:1;
+     y = 0:0.05:1;
+     for i=1:20
+        y(i) = c * log(1+x(i)) / (1+x(i)) ;
+     end
+     
+     figure(3);
+     plot(x,y);
+     
+     
+     figure(1);
+     hist(inv,20);
+     figure(2);
+     hist(rej,20);
 
-                %% Q9 : Exponentielle
-    exp=zeros(1,1000);
-    x = 0:0.01:5;
-    %exp2=pdf('exp',x,2);
-    exp2=exppdf(x,2);
-    for i=1:1000
-        exp(1,i) =Exponentielle(2);
-        
-    end
-
-    figure(1);
-    hist(exp,20);
-    figure(2);
-    plot(x, exp2);
+%                %% Q9 : Exponentielle
+%    exp=zeros(1,1000);
+%    x = 0:0.01:5;
+%    exp2=exppdf(x,1/2);
+%    for i=1:1000
+%        exp(1,i) =Exponentielle(2);
+%        
+%    end
+%
+%    figure(1);
+%    hist(exp,20);
+%    figure(2);
+%    plot(x, exp2);
 
 end
